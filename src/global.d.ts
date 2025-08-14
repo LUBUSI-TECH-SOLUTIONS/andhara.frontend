e8ad8c45061445e338e677cf25a8a185ffca5ec7
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-08-14 12:26:42 -0500

    build(app): Add config netlify for vite

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,7 @@ declare module '*.css';
 
 interface ImportMetaEnv {
   readonly VITE_API_URL_PROD: string;
+  readonly VITE_API_URL_DEV: string;
   // puedes agregar más si las tienes
   // readonly VITE_OTRA_VARIABLE: string;
 }
