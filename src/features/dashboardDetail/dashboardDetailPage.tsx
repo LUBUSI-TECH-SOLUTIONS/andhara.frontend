67f351b784c67d03433c8090488c277bb5245824
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-10-21 21:19:53 -0500

    build(app): comment out unused ProductSummaryHorizontal

--- src/features/dashboardDetail/dashboardDetailPage.tsx
+++ src/features/dashboardDetail/dashboardDetailPage.tsx
@@ -2,7 +2,6 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { SaleFilters } from "@/features/dashboardDetail/components/salesFIlters"
 import { GroupedIncomesTable } from "@/features/dashboardDetail/components/groupedSalesTable"
 import { SummaryCards } from "@/features/dashboardDetail/components/summaryCards"
-// import { ProductSummaryHorizontal } from "@/features/dashboardDetail/components/productSummaryHorizontal"
 
 export const DashboardDetailPage = () => {
   return (
@@ -16,12 +15,9 @@ export const DashboardDetailPage = () => {
         <CardContent className="space-y-4">
           <SaleFilters />
           <SummaryCards />
-          {/* <ProductSummaryHorizontal /> */}
           <GroupedIncomesTable />
         </CardContent>
-
       </Card>
-
     </section>
   )
 }
