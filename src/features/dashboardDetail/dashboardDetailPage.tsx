858f33138c1eb1cee2321381f497e0218ab66a5a
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-10-21 21:29:16 -0500

    feat(app): update DashboardDetailPage title to improve clarity

--- src/features/dashboardDetail/dashboardDetailPage.tsx
+++ src/features/dashboardDetail/dashboardDetailPage.tsx
@@ -2,7 +2,6 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { SaleFilters } from "@/features/dashboardDetail/components/salesFIlters"
 import { GroupedIncomesTable } from "@/features/dashboardDetail/components/groupedSalesTable"
 import { SummaryCards } from "@/features/dashboardDetail/components/summaryCards"
-import { ProductSummaryHorizontal } from "@/features/dashboardDetail/components/productSummaryHorizontal" 
 
 export const DashboardDetailPage = () => {
   return (
@@ -16,7 +15,6 @@ export const DashboardDetailPage = () => {
         <CardContent className="space-y-4">
           <SaleFilters />
           <SummaryCards />
-          <ProductSummaryHorizontal />
           <GroupedIncomesTable />
         </CardContent>
 
