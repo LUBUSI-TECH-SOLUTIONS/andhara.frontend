b68ff7444803f8735e114a34f39b5978b35efc55
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-10-29 20:54:15 -0500

    feat(app): add ProductSummaryHorizontal component

--- src/features/dashboardDetail/dashboardDetailPage.tsx
+++ src/features/dashboardDetail/dashboardDetailPage.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { SaleFilters } from "@/features/dashboardDetail/components/salesFIlters"
 import { GroupedIncomesTable } from "@/features/dashboardDetail/components/groupedSalesTable"
 import { SummaryCards } from "@/features/dashboardDetail/components/summaryCards"
-import { ProductSummaryHorizontal } from "@/features/dashboardDetail/components/productSummaryHorizontal"
+import { ProductSummaryHorizontal } from "@/features/dashboardDetail/components/productsSummaryHorizontal"
 
 export const DashboardDetailPage = () => {
   return (
